5f98aaeb74ec1f812fed649c250de597752d8abe
Yeastplume <mcordner@gmail.com>
2017-08-24 11:14:35 +0100

    not building plugins if platform is windows

--- cuckoo-sys/src/build.rs
+++ cuckoo-sys/src/build.rs
@@ -17,10 +17,13 @@
 extern crate cmake;
 extern crate fs_extra;
 
+mod sanity;
+
 use cmake::Config;
 use std::{env,fs};
 use std::path::{PathBuf};
 use fs_extra::dir::*;
+use sanity::Finder;
 
 /// Tests whether source cuckoo directory exists
 
@@ -33,6 +36,15 @@ pub fn fail_on_empty_directory(name: &str){
 }
 
 fn main() {
+	let mut command_finder=Finder::new();
+	//dumb and quick test for windows, can parse later
+	let windows_sysinfo = command_finder.maybe_have("systeminfo");
+	if let Some(_) = windows_sysinfo {
+		//Windows plugins not supported for now.. bye!
+		return;
+	}	
+
+
 	fail_on_empty_directory("plugins/cuckoo");
 	let path_str = env::var("OUT_DIR").unwrap();
 	let mut out_path = PathBuf::from(&path_str);
